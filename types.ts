09f79d18cffcef78a0c4c4390af0381684307a59
Joona Piirainen <joona.piirainen@gmail.com>
2020-05-25 20:03:37 +0300

    feat: addCsgoMatch

--- types.ts
+++ types.ts
@@ -1,12 +1,12 @@
 //interface for csgo match
 export interface Match {
-  id: string
-  category: string
-  tournament: string
-  team1: string
-  team2: string
+  id?: string;
+  category: string;
+  tournament: string;
+  team1: string;
+  team2: string;
   odds: {
-    team1Win: number
-    team2Win: number
-  }
+    team1Win: number;
+    team2Win: number;
+  };
 }
